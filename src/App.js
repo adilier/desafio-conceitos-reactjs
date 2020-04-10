619e427dd774350d1def1eaa956bfb3a98456bac
Adler Santos Rodrigues <adlerdet@outlook.com>
2020-04-10 20:17:58 -0300

    Add form

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import "./styles.css";
 
 function App() {
   const [repositories, setRepositories] = useState([]);
+  const [title, setTitle] = useState("");
+  const [url, setUrl] = useState("");
+  const [techs, setTechs] = useState("");
 
   useEffect(() => {
     api.get('/repositories').then(res => {
@@ -14,12 +17,17 @@ function App() {
     });
   },[]);
 
-  async function handleAddRepository() {
+  async function handleAddRepository(e) {
+    e.preventDefault();
     const res = await api.post('/repositories', {
-      title: "Back-end com node",
-      url: "https://github.com/adilier/desafio-conceitos-node",
-      techs: ["Node", "Express", "TypeScript"]
+      title: title,
+      url: url,
+      techs: techs
     });
+
+    setTitle('')
+    setUrl('')
+    setTechs('')
 
     const repository = res.data;
 
@@ -38,21 +46,56 @@ function App() {
 
   return (
     <div className="container container-custom">
-        <ul data-testid="repository-list">
-          {
-            repositories.map(repository => (
-              <li key={repository.id} className="listItem">
-                <h2>{repository.title}</h2>
-                <p className="techs">{repository.techs.join(', ')}</p>
-                <a href={repository.url} className="url" target="_blank" rel="noopener noreferrer">Acessar repositório</a>
-                <button onClick={() => handleRemoveRepository(repository.id)}>
-                  Remover <FiTrash2 className="buttonIcon"/>
-                </button>
-              </li>
-            ))
-          }
-        </ul>
-      <button onClick={handleAddRepository}>Adicionar <FiPlus className="buttonIcon"/></button>
+      <form>
+        <div>
+          <label>
+            Nome
+          </label>
+          <input
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+            placeholder="Desafio React"
+          />
+        </div>
+        <div>
+          <label>
+            Url
+          </label>
+          <input
+            value={url}
+            onChange={e => setUrl(e.target.value)}
+            placeholder="https://github.com/adilier/desafio-conceitos-reactjs"
+          />
+        </div>
+        <div>
+          <label>
+            Técnologias
+          </label>
+          <input
+            value={techs}
+            onChange={e => setTechs(e.target.value)}
+            placeholder="Node, React, Javascript"
+          />
+        </div>
+        <div className="containerButton">
+          <button className="buttonForm" onClick={handleAddRepository}>Adicionar <FiPlus className="buttonIcon"/></button>
+        </div>
+      </form>
+      <ul data-testid="repository-list">
+        {
+          repositories.map(repository => (
+            <li key={repository.id} className="listItem">
+              <h2>{repository.title}</h2>
+              <p className="techs">{repository.techs}</p>
+              <a href={repository.url} className="url" target="_blank" rel="noopener noreferrer">Acessar repositório</a>
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover <FiTrash2 className="buttonIcon"/>
+              </button>
+            </li>
+          ))
+        }
+      </ul>
+      
     </div>
   );
 }
